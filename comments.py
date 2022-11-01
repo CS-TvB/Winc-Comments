6a9f3776bc2cecd7a4ef548d73375279ae2a2fe3
Thomas van Boom <38430385+CS-TvB@users.noreply.github.com>
2022-11-01 09:23:49 +0100

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,17 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+#declare 5 to I
+I = 5
+print(I) #print the value declared to I (5)
+print("hallo winc") #this line prints "hallo winc"
+
+""""
+With 3 Apostrophe's
+you can comments on
+more 
+lines
+"""
